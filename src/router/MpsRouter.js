6dfc561a397cc3c78ffda33825d8b72f395e02cf
Paul Isoard <paulisoard2911@gmail.com>
2023-03-03 15:30:34 +0100

    router

--- src/router/MpsRouter.js
+++ src/router/MpsRouter.js
@@ -1,16 +1,26 @@
-import { Routes, Route } from 'react-router-dom'
-import LoginPage from '../pages/LoginPage'
-import RegisterPage from '../pages/RegisterPage'
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import HomePage from "../pages/HomePage";
+import SpotDetails from "../pages/SpotDetails";
+import ProfilePage from "../pages/ProfilePage"
+import FavoritePage from "../pages/FavoritePage"
 
 const MpsRouter = () => {
-    return(
-        <div>
-            <Routes>
-                <Route path='/' element={<LoginPage/>} />
-                <Route path='/register' element={<RegisterPage />} />
-            </Routes>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
 
-export default MpsRouter
+        {/* Protected routes */}
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/user/:userId" element={<ProfilePage />} />
+        <Route path="/spots/:spotId" element={<SpotDetails />} />
+        <Route path="/favorites" element={<FavoritePage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default MpsRouter;
