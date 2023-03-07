054775c7e17b47aed5b85e65f0de31a4896fb162
Paul Isoard <paulisoard2911@gmail.com>
2023-03-07 14:02:13 +0100

    <UserFunctions />

--- src/logic/UserFunctions.js
+++ src/logic/UserFunctions.js
@@ -1,11 +1,28 @@
-export const register = abc => {
-    return(
-        <div></div>
-    )
-}
+import axios from "axios";
 
-export const login = user => {
-    return(
-        <div></div>
-    )
-}
+export const register = (abc) => {
+  return axios.post("http://localhost:8000/auth/register", {
+    first_name: abc.first_name,
+    last_name: abc.last_name,
+    username: abc.username,
+    email: abc.email,
+    password: abc.password,
+    level: abc.level
+  })
+  .then(res => console.log('Registered'))
+  .catch(err => console.log(err))
+};
+
+export const login = (user) => {
+  return axios
+    .post("http://localhost:8000/auth", {
+      email: user.email,
+      password: user.password,
+    })
+    .then((res) => {
+      console.log(res.headers.get("auth-token"));
+      localStorage.setItem("usertoken", res.data);
+      return res.data;
+    })
+    .catch((err) => console.error(err));
+};
