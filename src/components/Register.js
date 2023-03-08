240c56e859c3a2e127953583ce4a1ada38fece30
Paul Isoard <paulisoard2911@gmail.com>
2023-03-08 16:54:01 +0100

    <Register /> + Register.css

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,20 @@
+import '../css/Register.css'
+
 const Register = () => {
-    return(
-        <div>
+  return (
+    <div className="div">
+      <form className="form">
+        <p className="heading">LOGIN</p>
+        <input className="input" type="text" placeholder="First Name"></input>
+        <input className="input" type="text" placeholder="Last Name"></input>
+        <input className="input" type="text" placeholder="Username"></input>
+        <input className="input" type="text" placeholder="Email"></input>
+        <input className="input" type="text" placeholder="Password"></input>
+        <input className="input" type="number" placeholder="Level"></input>
+        <button className="btn">Submit</button>
+      </form>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Register
+export default Register;
