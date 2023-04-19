0b64795b11acc8c15c00bb70e34399cf9aff924e
Paul Isoard <paulisoard2911@gmail.com>
2023-04-19 17:06:44 +0200

    <EditLevelPage /> + <EditLevel /> + css en cours

--- src/components/EditLevel.js
+++ src/components/EditLevel.js
@@ -1,7 +1,25 @@
-const EditLevel = () => {
-    return (
-        <div></div>
-    )
-}
+import "../css/EditLevel.css";
 
-export default EditLevel
+const EditLevel = () => {
+  return (
+    <div className="div-edit-level">
+      <p style={{fontSize: '1.5em', margin: '2.5% 0%'}}>How to choose your Level ?</p>
+      <p className="levels-explained">
+        You need to choose your level between four levels. Level one is for
+        beginners, level two is for intermediate surfers, level three for
+        advance surfers ans level four for experts. Your level was automatically
+        set up at one. To change your level, simply click on the suitable level,
+        and then submit. Good surf !
+      </p>
+      <div className="div-edit-level-button">
+        <button className="edit-level-button">1</button>
+        <button className="edit-level-button">2</button>
+        <button className="edit-level-button">3</button>
+        <button className="edit-level-button">4</button>
+      </div>
+      <button className="submit-level-button">Submit</button>
+    </div>
+  );
+};
+
+export default EditLevel;
--- src/pages/EditLevelPage.js
+++ src/pages/EditLevelPage.js
@@ -1,11 +1,22 @@
-import NavBar from "../components/NavBar"
-import Footer from "../components/Footer"
-import EditLevel from "../components/EditLevel"
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import EditLevel from "../components/EditLevel";
+import '../css/EditLevelPage.css'
 
 const EditLevelPage = () => {
-    return (
-        <div></div>
-    )
-}
+  return (
+    <div className="edit-level-page">
+      <div className="nav-bar">
+        <NavBar />
+      </div>
+      <div className="edit-level-component">
+        <EditLevel />
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
-export default EditLevelPage
+export default EditLevelPage;
