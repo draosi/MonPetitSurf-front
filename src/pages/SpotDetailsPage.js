535f914e52d7a818af0f25b083ab3aaeee7c725f
Paul Isoard <paulisoard2911@gmail.com>
2023-05-03 14:51:32 +0200

    <SpotDetailsPage /> + <RemoveFavorite />

--- src/pages/SpotDetailsPage.js
+++ src/pages/SpotDetailsPage.js
@@ -1,31 +1,39 @@
 import SurfMap from "../components/SurfMap";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import SpotInfos from "../components/SpotInfos";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-import Meteo from '../components/Meteo'
+import Meteo from "../components/Meteo";
 import AddFavorite from "../components/AddFavorite";
-import "../css/SpotDetailsPage.css"
+import RemoveFavorite from "../components/RemoveFavorite";
+import "../css/SpotDetailsPage.css";
 import { useContext } from "react";
 import { MpsContext } from "../context/MpsContext";
 
-const SpotdetailsPage = () => {
-  const {value5, value7, value10} = useContext(MpsContext)
-  const [spots, setSpots] = value5
-  const [query, setQuery] = value7
-  const [favoriteSpots, setFavoriteSpots] = value10
+const SpotDetailsPage = () => {
+  const { value5, value7, value10 } = useContext(MpsContext);
+  const [spots, setSpots] = value5;
+  const [query, setQuery] = value7;
+  const [favoriteSpots, setFavoriteSpots] = value10;
 
-  const spotToAdd = spots.filter((el) => el.spot_name === query)
-  console.log(spotToAdd)
+  const spotToAdd = spots.filter((el) => el.spot_name === query);
+  console.log(spotToAdd);
 
   const addFavoriteSpot = (spotToAdd) => {
-    const newFavoriteArray = [...favoriteSpots, spotToAdd]
-    console.log(newFavoriteArray)
-    setFavoriteSpots(newFavoriteArray)
-    alert("Spot added to your favorites !")
-  }
-  
-  console.log(favoriteSpots)
+    const newFavoriteArray = [...favoriteSpots, spotToAdd];
+    console.log(newFavoriteArray);
+    setFavoriteSpots(newFavoriteArray);
+    alert("Spot added to your favorites !");
+  };
+
+  const removeFavoriteSpot = (spotToAdd) => {
+    const newFavoriteArray = favoriteSpots.filter(
+      (el) => el.spot_name !== spotToAdd[0].spot_name
+    );
+    setFavoriteSpots(newFavoriteArray);
+    alert("Spot removed from your favorite !");
+  };
+
+  console.log(favoriteSpots);
 
   return (
     <div className="spot-details">
@@ -40,9 +48,47 @@ const SpotdetailsPage = () => {
           <div className="meteo">
             <Meteo />
           </div>
-          <div className="icon" onClick={() => addFavoriteSpot(spotToAdd)}>
-            <AddFavorite />
-          </div>
+          {/* {favoriteSpots.length !== 0 ? (
+            favoriteSpots[0].map((e) => {
+              e.spot_name === spotToAdd[0].spot_name ? (
+                <div
+                  className="icon"
+                  onClick={() => removeFavoriteSpot(spotToAdd)}
+                >
+                  <RemoveFavorite />
+                </div>
+              ) : (
+                <div
+                  className="icon"
+                  onClick={() => addFavoriteSpot(spotToAdd)}
+                >
+                  <AddFavorite />
+                </div>
+              );
+            })
+          ) : (
+            <div className="icon" onClick={() => addFavoriteSpot(spotToAdd)}>
+              <AddFavorite />
+            </div>
+          )} */}
+          {favoriteSpots.length !== 0 ? (
+            favoriteSpots[0]
+              .filter((e) => e.spot_name == spotToAdd[0].spot_name)
+              .map((el) => {
+                return (
+                  <div
+                    className="icon"
+                    onClick={() => removeFavoriteSpot(spotToAdd)}
+                  >
+                    <RemoveFavorite />
+                  </div>
+                );
+              })
+          ) : (
+            <div className="icon" onClick={() => addFavoriteSpot(spotToAdd)}>
+              <AddFavorite />
+            </div>
+          )}
         </div>
         <div className="graphique">
           <SpotInfos />
@@ -55,4 +101,4 @@ const SpotdetailsPage = () => {
   );
 };
 
-export default SpotdetailsPage;
+export default SpotDetailsPage;
